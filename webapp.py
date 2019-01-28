2e1e1edf4aa6bae51a01abc9e37cd526c47ec7c1
P3 <668292@my.sbunified.org>
2019-01-28 11:13:18 -0800

    finished

--- webapp.py
+++ webapp.py
@@ -14,11 +14,13 @@ app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.
 
 @app.route('/')
 def renderMain():
+    session.clear()
     return render_template('home.html')
+   
 
 @app.route('/startOver')
 def startOver():
-    #TODO: delete everything from the session
+    
     session.clear()
     return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
 
@@ -28,34 +30,43 @@ def renderPage1():
 
 @app.route('/page2',methods=['GET','POST'])
 def renderPage2():
-    #TODO: set the first and last name in the session
-    session["answer1"] = request.form["answer1"] 
-    if session["answer1"] == "french" or  session["answer1"] == "French":
-       session["answerscorrect"]= 1
+    
+    if "answer1" in session:
+        print(session["answerscorrect"])
     else:
-       session["answerscorrect"]= 0
-    print(session["answerscorrect"])
+        session["answer1"] = request.form["answer1"] 
+        if session["answer1"] == "french" or  session["answer1"] == "French":
+            session["answerscorrect"]= 1
+        else:
+            session["answerscorrect"]= 0
+    
     return render_template('page2.html')
 
 @app.route('/page3',methods=['GET','POST'])
 def renderPage3():
-    #TODO: set the favorite color in the session
-    session["answer2"] = request.form["answer2"] 
-    if session["answer2"] == 6:
-       session["answerscorrect"]= session["answerscorrect"] + 1
+    if "answer2" in session:
+        print(session["answerscorrect"])
     else:
-       session["answerscorrect"]= session["answerscorrect"]
+        session["answer2"] = request.form["answer2"] 
+        if session["answer2"] == "6":
+            session["answerscorrect"]= session["answerscorrect"] + 1
+        else:
+            session["answerscorrect"]= session["answerscorrect"]
     print(session["answerscorrect"])
     return render_template('page3.html')
     
 @app.route('/page4',methods=['GET','POST'])
 def renderPage4():
-    session["answer3"] = request.form["answer3"] 
-    if session["answer3"] == "spiders" or session["answer3"] == "Spiders" :
-       session["answerscorrect"]= session["answerscorrect"] + 1
+    if "answer3" in session:
+        print(session["answerscorrect"])
     else:
-       session["answerscorrect"]= session["answerscorrect"]
+        session["answer3"] = request.form["answer3"] 
+        if session["answer3"] == "spiders" or session["answer3"] == "Spiders" :
+            session["answerscorrect"]= session["answerscorrect"] + 1
+        else:
+            session["answerscorrect"]= session["answerscorrect"]
     print(session["answerscorrect"])
+    
     return render_template('page4.html')
 if __name__=="__main__":
     app.run(debug=True)
